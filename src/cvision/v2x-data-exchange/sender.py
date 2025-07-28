e3487d4a15af9032cf434667f0592bc2f38ae65e
debipe20 <debashisdas@arizona.edu>
2025-07-28 14:36:46 -0500

    updated code to forward latest v2x data

--- src/cvision/v2x-data-exchange/sender.py
+++ src/cvision/v2x-data-exchange/sender.py
@@ -1,21 +1,39 @@
 import os
+import platform
 import socket
 import time
+import json
 import firebase_admin
 from firebase_admin import credentials, db
 
 # Load the Firebase service account key
-sevrice_account_path = os.path.expanduser("~") + "/Documents/cvision-firebase-key.json"
-cred = credentials.Certificate(sevrice_account_path)
+current_os = platform.system()
+
+if current_os == "Linux":
+    service_account_path = os.path.expanduser("~") + "/Documents/cvision-firebase-key.json"
+    config_file_path = os.path.join(os.path.expanduser("~"), "Desktop", "debashis-workspace", "config", "anl-master-config.json")
+    
+elif current_os == "Windows":  # For Windows
+    service_account_path = os.path.join(os.path.expanduser("~"), "Documents", "cvision-firebase-key.json")
+    config_file_path = os.path.join("C:\\", "Users", "Documents", "debashis-workspace", "config", "anl-master-config.json")
+        
+else:
+    raise OSError(f"Unsupported operating system: {current_os}")
+
+cred = credentials.Certificate(service_account_path)
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://c-vision-7e1ec-default-rtdb.firebaseio.com/'
 })
 
 def main():
-    hostIp = "192.168.26.103"
-    port = 50002
+    config_file = open(config_file_path, "r")
+    config = json.load(config_file)
+    config_file.close()
+    
+    host_ip = config["IPAddress"]["HostIp"]
+    port = config["PortNumber"]["V2XDataSender"]
     msgReceiverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    msgReceiverSocket.bind((hostIp, port))
+    msgReceiverSocket.bind((host_ip, port))
 
     payload_prefix = "Payload="
     map_identifier = "0012"
@@ -23,7 +41,7 @@ def main():
     bsm_identifier = "0014"
     
 
-    print(f"📡 Listening on {hostIp}:{port}")
+    print(f"📡 Listening on {host_ip}:{port}")
 
     while True:
         try:
@@ -39,17 +57,17 @@ def main():
             print(f"📥 Received payload: {payload}")
 
             # Detect payload type
-            if payload.startswith(bsm_identifier):
-                ref = db.reference('/BSMData')
-                msg_type = "BSM"
-
-            elif payload.startswith("0012"):
+            if payload.startswith(map_identifier):
                 ref = db.reference('/MAPData')
                 msg_type = "MAP"
 
             elif payload.startswith(spat_identifier):
                 ref = db.reference('/SPaTData')
                 msg_type = "SPaT"
+                
+            elif payload.startswith(bsm_identifier):
+                ref = db.reference('/BSMData')
+                msg_type = "BSM"
             
             else:
                 print("⚠️ Unknown payload type, skipping...")
@@ -57,6 +75,14 @@ def main():
 
             # Send to Firebase
             ref.set({
+                "timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
+                "payload": payload
+            })
+            
+            # Send to unified /LatestV2XMessage
+            ref_latest = db.reference('/LatestV2XMessage')
+            ref_latest.set({
+                "type": msg_type,
                 "timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
                 "payload": payload
             })
